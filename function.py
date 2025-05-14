a94864713e809a4d087b22e473220a9d447a7d53
NeemaMwende <gracepete12@gmail.com>
2025-05-14 17:17:11 +0300

    functions recap

--- function.py
+++ function.py
@@ -33,11 +33,11 @@
 # # ask_ok('Are you sure you are okay?')
 # ask_ok('OK to overwrite the file?', 2, 'Come on, only yes or no!')
 
-def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
-    print("-- This parrot wouldn't", action, end=' ')
-    print("if you put", voltage, "volts through it.")
-    print("-- Lovely plumage, the", type)
-    print("-- It's", state, "!")
+# def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+#     print("-- This parrot wouldn't", action, end=' ')
+#     print("if you put", voltage, "volts through it.")
+#     print("-- Lovely plumage, the", type)
+#     print("-- It's", state, "!")
     
 # parrot(200)
 # parrot(1000)                                          # 1 positional argument
@@ -45,4 +45,36 @@ def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
 # parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
 # parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
 # parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
-parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+#parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+# def cheeseshop(kind, *arguments, **keywords):
+#     print("-- Do you have any", kind, "?")
+#     print("-- I'm sorry, we're all out of", kind)
+#     for arg in arguments:
+#         print(arg)
+#     print("-" * 40)
+#     for kw in keywords:
+#         print(kw, ":", keywords[kw])
+        
+# cheeseshop("Limburger", "It's very runny, sir.",
+#            "It's really very, VERY runny, sir.",
+#            shopkeeper="Michael Palin",
+#            client="John Cleese",
+#            sketch="Cheese Shop Sketch")
+
+def standard_arg(arg):
+    print(arg)
+
+def pos_only_arg(arg, /):
+    print(arg)
+
+def kwd_only_arg(*, arg):
+    print(arg)
+
+def combined_example(pos_only, /, standard, *, kwd_only):
+    print(pos_only, standard, kwd_only)
+    
+standard_arg(2)
+pos_only_arg(2)
+kwd_only_arg(arg=2)
+combined_example(1, standard=2, kwd_only=3)
